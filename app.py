5acd932e0291d81bf0924e45b1f7b2f739652b92
damelwani <devmelwani@gmail.com>
2025-12-24 14:26:40 -0500

    Update Flask app configuration settings

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ from helpers import login_required
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "longHomeworkSecretKey" 
 
+app.config["SESSION_TYPE"] = "null"
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 db = SQL("sqlite:///homework.db")
